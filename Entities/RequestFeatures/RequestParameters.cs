4dab00eedc3f48f81b5c95a477243e352b99aa7a
Wandu <wandumuzi.mathunjwa@investec.co..za>
2023-07-10 13:44:43 +0200

    Filtering Added. Start Chpt18

--- Entities/RequestFeatures/RequestParameters.cs
+++ Entities/RequestFeatures/RequestParameters.cs
@@ -21,5 +21,9 @@
 
     public class EmployeeParameters : RequestParameters
     {
+        public uint MinAge { get; set; }
+        public uint MaxAge { get; set; } = int.MaxValue;
+
+        public bool ValidAgeRange => MaxAge > MinAge;
     }
 }
